297d18cdb05054c27ca2ee4cdbd3ed0adfaa853a
acul71 <luca.pisani@birdo.net>
2021-05-05 01:43:00 +0200

    Add --help info

--- cidtree/cidcli.js
+++ cidtree/cidcli.js
@@ -2,19 +2,63 @@
 
 const meow = require('meow')
 const ct = require('./cidtree')
+/*
+USAGE
+  ipfs ls <ipfs-path>... - List directory contents for Unix filesystem objects.
 
+SYNOPSIS
+  ipfs ls [--headers | -v] [--resolve-type=false] [--size=false] [--stream | -s] [--] <ipfs-path>...
+
+ARGUMENTS
+
+  <ipfs-path>... - The path to the IPFS object(s) to list links from.
+
+OPTIONS
+
+  -v, --headers   bool - Print table headers (Hash, Size, Name).
+  --resolve-type  bool - Resolve linked objects to find out their types. Default: true.
+  --size          bool - Resolve linked objects to find out their file size. Default: true.
+  -s, --stream    bool - Enable experimental streaming of directory entries as they are traversed.
+
+DESCRIPTION
+
+  Displays the contents of an IPFS or IPNS object(s) at the given path, with
+  the following format:
+  
+    <link base58 hash> <link size in bytes> <link name>
+  
+  The JSON output contains type information.
+
+
+
+
+*/
 const cli = meow(`
-Usage 
-  $ codcli [.|files|dir] [options]
-  # codcli is a command line utility that helps you find/check/keep track/ of your files added on IPFS
-  # . is default files dir
+USAGE
+  cidcli file... - Check if a file is in IPFS
 
-Options 
+SYNOPSIS
+  cidcli [--cidVersion=1] [debug=false] <[.| files | dir]>
+  
+
+ARGUMENTS
+  . = current dir ('.' can be omitted)
+  file(s) = list of file(s)
+  directory(s) = list of directory(s)
+
+OPTIONS 
   cidVersion = [0,1] default=1
   debug = [true|false] default=false
-  
-Example
-  $ codcli bla bla bla
+
+DESCRIPTION
+  cidcli is a command line utility that helps you find/check/keep track/ of your files added on IPFS
+
+EXAMPLES
+  echo "hello world!" > test.txt
+  cidcli test.txt
+
+  test.txt CID(bafybeihp35x7qe7hn7g4eirzmb6dqnbw5z7x6kgko2ks4jzf5geqoirysy) PROVS=3
+
 `, {
   flags: {
     cidVersion: {
